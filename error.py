056bf5897ca5d6f328ae701de439b9d1c6ee0f64
CAMELAJANE2005 <152075751+CAMELAJANE2005@users.noreply.github.com>
2023-12-18 17:39:16 +0100

    Add files via upload

---
+++ error.py
@@ -0,0 +1,37 @@
+mylist=[14, "hello", 967] # Index was out of range 
+mylist[2]
+
+import pandas  # it was spelled wrongly
+import numpy
+
+
+print("python errors") # the print function was used wrongly
+
+
+
+
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True]   #The key "True" was not in the dictionary
+
+
+
+i= 79 # it was changed to avoid a bomb
+while i<78:   # The print(i) was not indented after the while function 
+    print(i)
+i+=1
+
+
+it=iter([1,2,3]) # the next function was used more than it was supposed to be
+next(it)
+next(it)
+next(it)
+
+
+15+15 # You cant add an integer with a string
+
+
+
+python = "Programming language" # the name python was not assigned to a variable
+
+
+x=19/3   # It was divided by zero which was wrong
